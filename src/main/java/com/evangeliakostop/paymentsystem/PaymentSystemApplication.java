6f80d8f108a266426632e5696507a51b51da8c56
Evangelia Kostopoulou <kostopevangelia@gmail.com>
2026-09-20 21:46:12 +0300

    exclude data source autoconfiguration form application

--- src/main/java/com/evangeliakostop/paymentsystem/PaymentSystemApplication.java
+++ src/main/java/com/evangeliakostop/paymentsystem/PaymentSystemApplication.java
@@ -3,11 +3,19 @@ package com.evangeliakostop.paymentsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class })
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		DataSourceAutoConfiguration.class
+})
+@EnableAutoConfiguration(exclude = {
+		SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class,
+		DataSourceAutoConfiguration.class
+})
 public class PaymentSystemApplication {
 
 	public static void main(String[] args) {
